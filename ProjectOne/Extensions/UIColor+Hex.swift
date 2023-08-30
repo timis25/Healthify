98294eed561cb1298b4ae82d8b0a8ff6c265a633
timis <timis252005@gmail.com>
2023-08-30 14:00:45 +0500

    fix

--- ProjectOne/Extensions/UIColor+Hex.swift
+++ ProjectOne/Extensions/UIColor+Hex.swift
@@ -8,20 +8,17 @@
 import UIKit
 
 extension UIColor {
-    convenience init(hex: String, alpha: CGFloat) {
-        let hexString: String = (hex as NSString).trimmingCharacters(in: .whitespacesAndNewlines)
-        let scanner = Scanner(string: hexString as String)
-        if hexString.hasPrefix("#") {
-            scanner.scanLocation = 1
-        }
-        var color: UInt32 = 0
-        scanner.scanHexInt32(&color)
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
 
-        let mask = 0x000000FF
+        var rgb: UInt64 = 0
 
-        let red   = CGFloat(Int(color >> 16) & mask) / 255.0
-        let green = CGFloat(Int(color >> 8) & mask) / 255.0
-        let blue  = CGFloat(Int(color) & mask) / 255.0
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgb & 0x0000FF) / 255.0
 
         self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
